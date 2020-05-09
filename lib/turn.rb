d83061df68ef22b28a8a139aa1e539b45242cac6
Rion Hutsell <st.gish@gmail.com>
2020-05-09 14:25:58 -0600

    Complete Iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,65 +9,84 @@ class Turn
 
   end
 
-   def type
+  def type
 
-     if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
 
-       return :war
+     return :war
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
 
-     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+     return :mutually_assured_destruction
+    else
 
-       return :mutually_assured_destruction
-
-     else
-
-
-       :basic
-     end
-
+     :basic
     end
-
+  end
 
 
   def winner
 
     if type == :basic
 
-        if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
 
-          return player1
-        elsif player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+       return player1
+      elsif player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
 
-        else
+      else
 
-          player2
-        end
+        player2
+      end
 
     elsif type == :war
 
-        if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
 
-          return player1
-        else
+        return player1
+      else
 
-          player2
-        end
-
-
+        player2
+      end
 
     else
 
       "No Winner"
     end
-
   end
 
   def pile_cards
 
+    if type == :basic
+
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+
+    elsif type == :war
+
+      3.times do
+        @spoils_of_war << player1.deck.remove_card
+        @spoils_of_war << player2.deck.remove_card
+
+      end
+
+    else
+
+      3.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+
+    end
+
   end
 
-  def award_spoils
+  def award_spoils(winner)
 
+
+    spoils_of_war.each do |card|
+      winner.deck.add_card(card)
+
+    end
   end
 
 
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -17,48 +17,217 @@ class TurnTest < Minitest::Test
     @card6 = Card.new(:diamond, 'Queen', 12)
     @card7 = Card.new(:heart, '3', 3)
     @card8 = Card.new(:diamond, '2', 2)
-
-    @deck1 = Deck.new(@card1, @card2, @card5, @card8)
-    @deck2 = Deck.new(@card3, @card4, @card6, @card7)
-
-    @player1 = Player.new("Megan", @deck1)
-    @player2 = Player.new("Aurora", @deck2)
-
-    @turn = Turn.new(@player1, @player2)
+    @card9 = Card.new(:diamond, '8', 8)
 
   end
 
   def test_it_exists
 
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card3, @card4, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
-    assert_instance_of Turn, @turn
+    assert_instance_of Turn, turn
   end
 
   def test_it_can_have_players
 
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card3, @card4, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
-    assert_equal @player1, @turn.player1
-    assert_equal @player2, @turn.player2
+
+    assert_equal player1, turn.player1
+    assert_equal player2, turn.player2
   end
 
   def test_spoils_of_war_is_an_empty_array
 
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card3, @card4, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
-    assert_equal [], @turn.spoils_of_war
+
+    assert_equal [], turn.spoils_of_war
   end
 
-  def test_it_can_tell_type
+  def test_it_can_tell_type_basic
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card3, @card4, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
 
-    assert_equal :basic, @turn.type
+    assert_equal :basic, turn.type
   end
 
-  def test_it_can_tell_winner
+  def test_it_can_tell_type_war
 
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
-    assert_equal @player1, @turn.winner
+    assert_equal :war, turn.type
   end
 
+  def test_it_can_tell_type_mutually_assured_destruction
 
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card9, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+
+    assert_equal :mutually_assured_destruction, turn.type
+  end
+
+  def test_it_can_tell_winner_basic
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card3, @card4, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+
+
+    assert_equal player1, turn.winner
+  end
+
+  def test_it_can_tell_winner_war
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal player2, turn.winner
+  end
+
+  def test_it_can_tell_no_winner
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card9, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal "No Winner", turn.winner
+  end
+
+  def test_it_can_pile_cards_basic
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card3, @card4, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+
+    turn.pile_cards
+
+
+    assert_equal [@card1, @card3], turn.spoils_of_war
+
+  end
+
+  def test_it_can_pile_cards_war
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+
+    turn.pile_cards
+
+
+    assert_equal [@card1, @card4, @card2, @card3, @card5, @card6], turn.spoils_of_war
+
+  end
+
+  def test_it_removes_cards_if_mutually_assured_destruction
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card9, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+
+
+    assert_equal [@card8], player1.deck.cards
+    assert_equal [@card7], player2.deck.cards
+  end
+
+  def test_it_can_award_spoils_basic
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card3, @card4, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+
+    winner = turn.winner
+    turn.pile_cards
+
+
+    turn.award_spoils(winner)
+
+
+    assert_equal [@card2, @card5, @card8, @card1, @card3], player1.deck.cards
+    assert_equal [@card4, @card6, @card7], player2.deck.cards
+  end
+
+  def test_it_can_award_spoils_war
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+
+    winner = turn.winner
+    turn.pile_cards
+
+    turn.award_spoils(winner)
+
+
+    assert_equal [@card7, @card1, @card4, @card2, @card3, @card5, @card6], player2.deck.cards
+    assert_equal [@card8], player1.deck.cards
+  end
+
+  def test_it_will_award_no_spoils_war_if_mutually_assured_destruction
+
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card9, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    winner = turn.winner
+    turn.pile_cards
+
+    turn.award_spoils(winner)
+
+    assert_equal [@card8], player1.deck.cards
+    assert_equal [@card7], player2.deck.cards
+  end
 
 end
